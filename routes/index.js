97d8fe0eb23824b955c16115687e832f513bab15
Jovan Milicevic <jovan@kaywa.com>
2015-12-07 16:13:07 +0100

    let's tell a great story

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+    res.render('home', {title:'Userstory MVP'});
 });
-
 module.exports = router;
